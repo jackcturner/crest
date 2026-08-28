75a0d7766ae5f80e94c73fdeedb72ff401d8c15f
jturner200 <jack.turner200@gmail.com>
2026-08-28 10:31:36 +0100

    Add GAIA functions to __init__.

--- crest/imaging/__init__.py
+++ crest/imaging/__init__.py
@@ -1,9 +1,11 @@
 from crest.imaging.background import Background, block_validate, distance_validate
 from crest.imaging.psf import PSF
-from crest.imaging.masking import create_edge_mask, regions_to_mask, clean_regions
+from crest.imaging.masking import (create_edge_mask, regions_to_mask, clean_regions, 
+                                   gaia_query_to_regions, gaia_table_to_regions)
 from crest.imaging.completeness import measure_completeness
 from crest.imaging.utils import rebin_image, generate_error_map, create_stack
 
 __all__ = ['Background', 'block_validate', 'distance_validate', 'PSF',
-           'create_edge_mask', 'regions_to_mask', 'clean_regions', 'measure_completeness',
+           'create_edge_mask', 'regions_to_mask', 'clean_regions', 'gaia_query_to_regions',
+           'gaia_table_to_regions', 'measure_completeness',
            'rebin_image', 'generate_error_map', 'create_stack']
